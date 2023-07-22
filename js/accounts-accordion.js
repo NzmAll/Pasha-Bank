f9660de79daa4a0ef65e66b17e923e6231d8b024
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2023-07-22 17:28:21 +0400

    Accordion JS added

---
+++ js/accounts-accordion.js
@@ -0,0 +1,33 @@
+export function accAccordion() {
+  const accordionItems = document.querySelectorAll(".accordion-item");
+
+  accordionItems.forEach((accordionItem, index) => {
+    let header = accordionItem.querySelector(".accordion-header");
+    header.addEventListener("click", function () {
+      accordionItem.classList.toggle("open");
+
+      let description = accordionItem.querySelector(".accordion-content");
+      if (accordionItem.classList.contains("open")) {
+        description.style.display = "block";
+        header.querySelector("i").classList.replace("fa-plus", "fa-minus");
+        header.style.color = "green";
+      } else {
+        description.style.display = "none";
+        header.querySelector("i").classList.replace("fa-minus", "fa-plus");
+      }
+      removeOpen(index);
+    });
+  });
+
+  function removeOpen(index1) {
+    accordionItems.forEach((item2, index2) => {
+      if (index1 !== index2) {
+        item2.classList.remove("open");
+
+        let des = item2.querySelector(".accordion-content");
+        des.style.display = "none";
+        item2.querySelector("i").classList.replace("fa-minus", "fa-plus");
+      }
+    });
+  }
+}
